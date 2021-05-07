ab161a5ce10e757493a9b065116f3f690833c5db
zhzhao <zhzhao999@126.com>
2021-05-07 17:55:21 +0800

    第二种设计模式

--- src/main/java/top/zhzhao/patterns/p02_FactoryMethod/test.java
+++ src/main/java/top/zhzhao/patterns/p02_FactoryMethod/test.java
@@ -1,4 +1,10 @@
 package top.zhzhao.patterns.p02_FactoryMethod;
 
+import org.junit.Test;
+
 public class test {
+    @Test
+    public void test01(){
+        System.out.println("Hello world");
+    }
 }
